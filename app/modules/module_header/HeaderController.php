28bd4c73f2ace3140311defc54e77229d41b129e
Роман <roman-li-83@yandex.ru>
2025-03-20 22:54:56 +0500

    replace ModuleBuilderInterface to core/Builder

--- app/modules/module_header/HeaderController.php
+++ app/modules/module_header/HeaderController.php
@@ -3,14 +3,24 @@
 namespace Module_Header;
 use Core\Builder\Module;
 use Core\Builder\ModuleBuilder;
+use Core\Builder\NewModuleBuilder;
 
 class HeaderController
 {
-    public static function index()
+   /*
+   public static function index()
     {
         $headerBuilder = new ModuleBuilder(new Module());
         $headerBuilder
             ->setHtml('header')
             ->build();
     }
+   */
+    public static function index(){
+        $headerBuilder = new NewModuleBuilder();
+        $headerBuilder
+            ->setHtml('headerNew');
+        NewModuleBuilder::build();
+    }
+
 }
