856db689b62bcd023d38ce524697567c1f99fc58
Roman <roman_usmga@mail.ru>
2025-07-31 16:51:57 +0500

    include "Router.php" from catalog "Core\Routing_v2"

--- index.php
+++ index.php
@@ -2,8 +2,12 @@
 require_once "core/composer/vendor/autoload.php";
 
 use Core\Libs\DotEnv;
-use Core\Routing\Router;
+use Core\Routing_v2\Router;
+
+//use Core\Routing\Router;
 
 Dotenv::dotEnv();
+//$router = new Router();
 $router = new Router();
+
 //phpinfo();
