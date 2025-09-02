2bddf0c37ff17d9b539988ad1fd4ea6d01df22e3
Роман <roman-li-83@yandex.ru>
2025-09-02 23:34:33 +0500

    replace from 'index.php' to RouteAction class

--- index.php
+++ index.php
@@ -3,38 +3,21 @@ require_once "core/composer/vendor/autoload.php";
 
 use Core\Libs\DotEnv;
 use Core\Routing_v2\Route;
+use Core\Routing_v2\RouteAction;
 use Core\Routing_v2\Router;
 use Core\TestPagePublic\TestPagePublicController;
 
 //use Core\Routing\Router;
 
 Dotenv::dotEnv();
+
+//for Routing
 //$router = new Router();
 
-$router = new Router([
-    new Route('test_page_new', '/test_page_new', [TestPagePublicController::class]),
-]);
-
-
-
-try {
-
-    $route = $router->matchFromPath($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
-    $params = $route->getParams();
-    $args = $route->getVars();
-
-    $controllerName = $params[0];
-    $methodName = $params[1]??null;
-
-    $controller = new $controllerName();
-    if (!is_callable($controller)){
-        $controller = [$controller,$methodName];
-    }
-    echo $controller(...array_values($args));
-}catch (Exception $exception){
-    header("HTTP/1.0 404 Not Found");
-    //echo "HTTP/1.0 404 Not Found";
-}
+/**
+ * for Routing_v2
+ */
+$routeAction = new RouteAction();
+$routeAction->init();
 
 //phpinfo();
-//test git connect
