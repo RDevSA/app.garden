671abd41f4da8e928ba076f5e468d5be6839aa9c
Roman <roman_usmga@mail.ru>
2025-09-03 14:09:05 +0500

    optimized imports

--- index.php
+++ index.php
@@ -2,10 +2,7 @@
 require_once "core/composer/vendor/autoload.php";
 
 use Core\Libs\DotEnv;
-use Core\Routing_v2\Route;
 use Core\Routing_v2\RouteAction;
-use Core\Routing_v2\Router;
-use Core\TestPagePublic\TestPagePublicController;
 
 //use Core\Routing\Router;
 
